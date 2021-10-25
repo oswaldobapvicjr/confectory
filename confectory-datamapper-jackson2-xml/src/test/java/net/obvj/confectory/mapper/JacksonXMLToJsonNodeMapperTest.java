fbcaa14d7a938d139b99f14f5c73fee874f47498
Oswaldo Junior <oswaldo.bapvic.jr@outlook.com>
2021-10-25 17:07:17 -0300

    First implementation draft for enhancement #15

---
+++ confectory-datamapper-jackson2-xml/src/test/java/net/obvj/confectory/mapper/JacksonXMLToJsonNodeMapperTest.java
@@ -0,0 +1,67 @@
+package net.obvj.confectory.mapper;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import net.obvj.confectory.helper.JacksonJsonNodeHelper;
+
+/**
+ * Unit tests for the {@link JacksonXMLToJsonNodeMapper} class.
+ *
+ * @author oswaldo.bapvic.jr
+ * @since 0.3.0
+ */
+@ExtendWith(MockitoExtension.class)
+class JacksonXMLToJsonNodeMapperTest
+{
+    private static final String TEST_XML_SAMPLE1
+            = "<root>\n"
+            + "  <intValue>9</intValue>\n"
+            + "  <booleanValue>true</booleanValue>\n"
+            + "  <array>\n"
+            + "    <element>string1</element>\n"
+            + "    <element>string2</element>\n"
+            + "  </array>\n"
+            + "</root>\n";
+
+    @Mock
+    private JsonNode jsonNode;
+
+    private JacksonXMLToJsonNodeMapper mapper = new JacksonXMLToJsonNodeMapper();
+
+    private ByteArrayInputStream toInputStream(String content)
+    {
+        return new ByteArrayInputStream(content.getBytes());
+    }
+
+    @Test
+    void apply_validInputStream_validJsonNode() throws IOException
+    {
+        JsonNode result = mapper.apply(toInputStream(TEST_XML_SAMPLE1));
+        assertThat(result.size(), equalTo(3));
+        assertThat(result.get("intValue").asInt(), equalTo(9));
+        assertThat(result.get("booleanValue").asBoolean(), equalTo(true));
+
+        JsonNode array = result.get("array").get("element");
+        assertThat(array.size(), equalTo(2));
+        assertThat(array.get(0).asText(), equalTo("string1"));
+        assertThat(array.get(1).asText(), equalTo("string2"));
+    }
+
+    @Test
+    void configurationHelper_jsonObjectConfigurationHelper()
+    {
+        assertThat(mapper.configurationHelper(jsonNode).getClass(), equalTo(JacksonJsonNodeHelper.class));
+    }
+
+}
