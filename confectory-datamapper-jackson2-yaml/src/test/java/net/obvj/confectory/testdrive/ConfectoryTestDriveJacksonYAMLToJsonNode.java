723a08214e0029a84e9a0c35fd13cd45a38a6957
Oswaldo Baptista Vicente Junior <45291656+oswaldobapvicjr@users.noreply.github.com>
2021-10-29 21:02:43 -0300

    Fix issue #43 (#44)

---
+++ confectory-datamapper-jackson2-yaml/src/test/java/net/obvj/confectory/testdrive/ConfectoryTestDriveJacksonYAMLToJsonNode.java
@@ -0,0 +1,25 @@
+package net.obvj.confectory.testdrive;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import net.obvj.confectory.Configuration;
+import net.obvj.confectory.mapper.JacksonYAMLToJsonNodeMapper;
+import net.obvj.confectory.source.SourceFactory;
+
+public class ConfectoryTestDriveJacksonYAMLToJsonNode
+{
+    public static void main(String[] args)
+    {
+        String yaml = "root:\n"
+                    + "  foo: bar\n";
+
+        Configuration<JsonNode> config = Configuration.<JsonNode>builder()
+                .namespace("test")
+                .source(SourceFactory.stringSource(yaml))
+                .mapper(new JacksonYAMLToJsonNodeMapper())
+                .build();
+
+        System.out.println(config.getBean().get().toPrettyString());
+        System.out.println(config.getString("$.root.foo"));
+    }
+}
