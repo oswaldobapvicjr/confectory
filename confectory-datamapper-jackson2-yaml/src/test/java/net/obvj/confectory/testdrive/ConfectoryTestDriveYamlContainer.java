d7d25f5877434ecd22e0e8accc74000097e58e11
Oswaldo Junior <oswaldo.bapvic.jr@outlook.com>
2021-11-23 00:55:31 -0300

    Update ConfectoryTestDriveYamlContainer.java

--- confectory-datamapper-jackson2-yaml/src/test/java/net/obvj/confectory/testdrive/ConfectoryTestDriveYamlContainer.java
+++ confectory-datamapper-jackson2-yaml/src/test/java/net/obvj/confectory/testdrive/ConfectoryTestDriveYamlContainer.java
@@ -11,17 +11,22 @@ public class ConfectoryTestDriveYamlContainer
 {
     public static void main(String[] args)
     {
-        String yaml1 = "enabled: false\nstring: value1";
-        String yaml2 = "enabled: true\nstring: value2";
+        String yaml1 = "enabled: false\n"
+                     + "string: value1\n";
+        String yaml2 = "enabled: true\n"
+                     + "int: 77\n";
 
         ConfigurationContainer config = new ConfigurationContainer(
                 Configuration.<JsonNode>builder().source(SourceFactory.stringSource(yaml1))
-                        .mapper(new JacksonYAMLToJsonNodeMapper()).lazy().precedence(10).build(),
+                        .mapper(new JacksonYAMLToJsonNodeMapper()).lazy()
+                        .precedence(10).build(),
                 Configuration.<JsonNode>builder().source(SourceFactory.stringSource(yaml2))
-                        .mapper(new JacksonYAMLToJsonNodeMapper()).lazy().precedence(5).build());
+                        .mapper(new JacksonYAMLToJsonNodeMapper()).lazy()
+                        .precedence(5).build());
 
-        System.out.println(config.getBoolean("$.enabled"));
-        System.out.println(config.getBoolean("$.enablede"));
+        System.out.println(config.getBoolean("enabled"));
+        System.out.println(config.getString("string"));
+        System.out.println(config.getInteger("int"));
 
     }
 }
